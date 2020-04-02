cd7e5c8c1a086cc705e19170664c05591c9637d1
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-02 18:58:25 +0800

    Add input functions to main() in temperatures.py

--- Week 3 Files/temperatures.py
+++ Week 3 Files/temperatures.py
@@ -7,17 +7,17 @@ Github link: https://github.com/kkw123/CP1404Practicals
 """
 
 def main():
-    celsius_to_fahrenheit()
-    fahrenheit_to_celsius()
+    temp_celsius = float(input('Enter temperature in celsius: '))
+    temp_fahrenheit = float(input('Enter temperature in fahrenheit: '))
+    celsius_to_fahrenheit(temp_celsius)
+    fahrenheit_to_celsius(temp_fahrenheit)
 
 
-def celsius_to_fahrenheit():
-    temp = float(input('Enter temperature in celsius: '))
+def celsius_to_fahrenheit(temp):
     fahrenheit = (temp*1.8) + 32
     print('{:.2f}'u'\u2103'' is {:.2f}'u'\u2109'.format(temp,fahrenheit))
 
-def fahrenheit_to_celsius():
-    temp = float(input('Enter temperature in celsius: '))
+def fahrenheit_to_celsius(temp):
     celsius = (5*(temp-32))/9
     print('{:.2f}'u'\u2109'' is {:.2f}'u'\u2103'.format(temp, celsius))
 
