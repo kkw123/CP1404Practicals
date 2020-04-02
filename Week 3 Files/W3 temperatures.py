f29c69004b82aa1278f626314ce0db7abf421246
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-02 18:14:06 +0800

    Week 3 Temperature Change functions

---
+++ Week 3 Files/W3 temperatures.py
@@ -0,0 +1,17 @@
+"""
+CP1404 Practicals
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+
+"""
+
+def main():
+    celsius_to_fahrenheit()
+    fahrenheit_to_celsius()
+
+
+def celsius_to_fahrenheit():
+    temp = input('Enter temperature in celsius: ')
+    fahrenheit = (temp*1.8) + 32
+    print("{}"u"\u2103""is {}"u"\u2109".format(temp.fahrenheit))
