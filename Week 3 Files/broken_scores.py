32ec37c9f0085f01ecabe43e063bd2c9fcbc5498
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-02 18:47:21 +0800

    Add main() and checkscore() to broken_scores.py

--- Week 3 Files/broken_scores.py
+++ Week 3 Files/broken_scores.py
@@ -5,3 +5,26 @@ Name: Kum King Wye
 Github link: https://github.com/kkw123/CP1404Practicals
 
 """
+
+def main():
+    score = int(input("Enter your score: "))
+    result = checkScore(score)
+    print(result)
+
+
+def checkScore(score):
+    if score < 0 or score > 100:
+        return "Invalid Score"
+    elif score > 80:
+        return "A+"
+    elif score > 74:
+        return "A"
+    elif score > 64:
+        return "B"
+    elif score > 49:
+        return "C"
+    else:
+        return "Fail"
+
+
+main()
