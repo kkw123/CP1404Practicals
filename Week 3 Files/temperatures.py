3ee0964d872d5b69e8674fb2d98c742f68528374
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-07 20:35:41 +0800

    Fix function inputs

--- Week 3 Files/temperatures.py
+++ Week 3 Files/temperatures.py
@@ -9,10 +9,10 @@ Github link: https://github.com/kkw123/CP1404Practicals
 def main():
     original_celsius = float(input('Enter temperature in celsius: '))
     original_fahrenheit = float(input('Enter temperature in fahrenheit: '))
-    converted_to_fahrenheit = celsius_to_fahrenheit(temp_celsius)
-    converted_to_celsius = fahrenheit_to_celsius(temp_fahrenheit)
-    print('{} degrees celsius is {} degrees fahrenheit'.format(original_celsius,converted_to_fahrenheit))
-    print('{} degrees fahrenheit is {} degrees celsius'.format(original_fahrenheit,converted_to_celsius))
+    converted_to_fahrenheit = celsius_to_fahrenheit(original_celsius)
+    converted_to_celsius = fahrenheit_to_celsius(original_fahrenheit)
+    print('{:.2f} degrees celsius is {:.2f} degrees fahrenheit'.format(original_celsius , converted_to_fahrenheit))
+    print('{:.2f} degrees fahrenheit is {:.2f} degrees celsius'.format(original_fahrenheit , converted_to_celsius))
 
 
 def celsius_to_fahrenheit(temp):
