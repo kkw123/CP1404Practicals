b72479867b13d1f82c4dcf5e4a1be4a51f0b691d
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-07 20:51:05 +0800

    Create GPS file and added main()

---
+++ Week 3 Files/gopher_population_simulator.py
@@ -0,0 +1,25 @@
+"""
+CP1404 Practicals
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+
+"""
+
+def main():
+    population = 1000
+    max_year = 10
+    start_year = 1
+    min_birth_rate = 0.1
+    max_birth_rate = 0.2
+    min_death_rate = 0.05
+    max_death_rate = 0.25
+    while start_year < max_year + 1:
+        pass
+
+
+def calculate_births(min,max,population):
+    pass
+
+def calculate_deaths(min,max,population):
+    pass
