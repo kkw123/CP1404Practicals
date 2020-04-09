77ebec76208284d928898bff924b4fa4f41774a3
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-09 19:01:22 +0800

    Add make_draw() with unrepeated random number generator

--- Week 4 Files/quick_picks.py
+++ Week 4 Files/quick_picks.py
@@ -5,12 +5,25 @@ Name: Kum King Wye
 Github link: https://github.com/kkw123/CP1404Practicals
 
 """
+import random
 FIRST_NUMBER = 1
 LAST_NUMBER = 45
 NUMBER_PER_DRAW = 6
 
+
 def main():
-    pass
+    amt_of_draws = int(input('How many quick picks? '))
+
+
+
+def make_draw():
+    drawn_numbers = []
+    current_number = int()
+    while len(drawn_numbers) != NUMBER_PER_DRAW:
+        current_number = random.randint(FIRST_NUMBER-1,LAST_NUMBER)
+        if current_number not in drawn_numbers:
+            drawn_numbers.append(current_number)
+    print(drawn_numbers)
 
 
 main()
