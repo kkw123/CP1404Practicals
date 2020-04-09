11cce2e35a419bd0a8ea5cf6fcfc2768d7239c44
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-09 17:40:07 +0800

    Move income report to it's own function

--- Week 4 Files/total_income.py
+++ Week 4 Files/total_income.py
@@ -8,17 +8,22 @@ Github link: https://github.com/kkw123/CP1404Practicals
 
 
 def main():
-    """Display income report for incomes over a given number of months."""
+    """Ask how many months and the income for each month"""
     incomes = []
-    months = int(input("How many months? "))
+    total_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
+    for month in range(1, total_months + 1):
         income = float(input("Enter income for month {}: ".format(str(month))))
         incomes.append(income)
 
+    print_report(total_months, incomes)
+
+
+def print_report(total_months, incomes):
+    """Display income report"""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, total_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
