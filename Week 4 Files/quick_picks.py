ed500630e3ceabf87cddfd63ab09593fb68e8b83
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-09 18:42:27 +0800

    Add quick_picks.py

---
+++ Week 4 Files/quick_picks.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Practicals
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+
+"""
+FIRST_NUMBER = 1
+LAST_NUMBER = 45
+NUMBER_PER_DRAW = 6
+
+def main():
+    pass
+
+
+main()
