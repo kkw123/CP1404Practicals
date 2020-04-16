3b18de4e07244c676d9c26ea344e6166d5b7f518
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-16 16:35:14 +0800

    Allow small letters to be input

--- Week 5 Files/state_names.py
+++ Week 5 Files/state_names.py
@@ -15,8 +15,8 @@ print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ")
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+    if state_code.upper() in CODE_TO_NAME:
+        print('{:<} is {}'.format(state_code.upper(), CODE_TO_NAME[state_code.upper()]))
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ")
