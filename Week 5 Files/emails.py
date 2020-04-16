3672574e3ad1d340a4271f051c34d9f23604ca37
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-16 20:49:57 +0800

    Add function to ask user if their name is correct

--- Week 5 Files/emails.py
+++ Week 5 Files/emails.py
@@ -8,21 +8,35 @@ Github link: https://github.com/kkw123/CP1404Practicals
 
 
 def main():
+    name_to_email = {}
     input_email = input('Enter email: ')
     # ask user for email
     name_and_email = input_email.split('@')
     name = extract_name(name_and_email[0])
+    name = ask_name(name)
     # convert text in front of @ into a name
     # ask user if name is correct
     # if True add into dictionary, else ask for real name and store both
-    pass
+
 
 
 def extract_name(string):
     extracted_name = (' '.join(string.lower().split('.'))).title()
-    print(extracted_name)
+    # Makes every word a lower case letter
+    # Splits string with dots as delimiter
+    # Joins the array with spaces
+    # Makes the first letter of each word into capital letter
+    return extracted_name
 
 
-extract_name('linDsay.wArd')
+def ask_name(name):
+    while True:
+        input_answer = input('Is your name {}? (Y/n)'.format(name))
+        if input_answer.lower() == 'n':
+            name = input('Enter name: ')
+            return name
+        elif input_answer.upper == 'Y' or input_answer == '':
+            return name
+        print('Invalid Input')
 
 
