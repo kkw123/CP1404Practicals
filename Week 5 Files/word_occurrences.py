33af2402e25e3ca6fda2ba82133494162b918241
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-16 17:22:12 +0800

    Add print function that aligns nicely

--- Week 5 Files/word_occurrences.py
+++ Week 5 Files/word_occurrences.py
@@ -10,8 +10,9 @@ Github link: https://github.com/kkw123/CP1404Practicals
 def main():
     user_input = input('Enter a string: ').strip()
     counts_per_word = check_string(user_input)
+    longest_word = check_longest_word(counts_per_word)
     for word, count in counts_per_word.items():
-        print('{:{}} = {}'.format(word, 10, count))
+        print('{:{}} = {}'.format(word, len(longest_word), count))
 
 
 def check_string(string1):
@@ -28,7 +29,7 @@ def check_string(string1):
 def check_longest_word(dict1):
     longest_word = str()
     for word in dict1:
-        if len(word) > longest_word:
+        if len(word) > len(longest_word):
             longest_word = word
     return longest_word
 
