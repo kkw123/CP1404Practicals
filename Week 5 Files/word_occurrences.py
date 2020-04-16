efe833b50bc97eb51182afaa289c58fe7c5d113e
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-16 17:14:08 +0800

    Add an occurrence checker for strings

--- Week 5 Files/word_occurrences.py
+++ Week 5 Files/word_occurrences.py
@@ -9,12 +9,19 @@ Github link: https://github.com/kkw123/CP1404Practicals
 
 def main():
     user_input = input('Enter a string: ').strip()
-    print(check_string(user_input))
+    counts_per_word = check_string(user_input)
+    print(counts_per_word)
 
 
 def check_string(string1):
     words = string1.split(' ')
-
+    word_count = {}
+    for word in words:
+        if word not in word_count:
+            word_count[word] = 1
+        else:
+            word_count[word] += 1
+    return word_count
 
 
 main()
