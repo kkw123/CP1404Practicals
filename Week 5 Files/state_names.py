8d482fdbf69e9b9d9014087a846384dfa892fd78
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-16 16:41:59 +0800

    Add a function to print the whole dictionary

--- Week 5 Files/state_names.py
+++ Week 5 Files/state_names.py
@@ -13,10 +13,20 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code.upper() in CODE_TO_NAME:
-        print('{:<} is {}'.format(state_code.upper(), CODE_TO_NAME[state_code.upper()]))
-    else:
-        print("Invalid short state")
+
+def user_input(dict1):
     state_code = input("Enter short state: ")
+    while state_code != "":
+        if state_code.upper() in dict1:
+            print('{} is {}'.format(state_code.upper(), dict1[state_code.upper()]))
+        else:
+            print("Invalid short state")
+        state_code = input("Enter short state: ")
+
+
+def print_all(dict1):
+    for code, name in dict1.items():
+        print('{:<3} is {}'.format(code, name))
+
+
+print_all(CODE_TO_NAME)
