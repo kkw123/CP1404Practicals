ee32dc96b001f3473c3eef3a49c4f251a5fb8880
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-16 20:40:37 +0800

    Add function to extract name from input email

--- Week 5 Files/emails.py
+++ Week 5 Files/emails.py
@@ -8,8 +8,21 @@ Github link: https://github.com/kkw123/CP1404Practicals
 
 
 def main():
+    input_email = input('Enter email: ')
     # ask user for email
+    name_and_email = input_email.split('@')
+    name = extract_name(name_and_email[0])
     # convert text in front of @ into a name
     # ask user if name is correct
     # if True add into dictionary, else ask for real name and store both
     pass
+
+
+def extract_name(string):
+    extracted_name = (' '.join(string.lower().split('.'))).title()
+    print(extracted_name)
+
+
+extract_name('linDsay.wArd')
+
+
