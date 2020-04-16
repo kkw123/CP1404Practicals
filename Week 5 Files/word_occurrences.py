f21627605c0c33049cd6e476cd2c2d349249c749
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-16 17:59:33 +0800

    Change max word count function into a list comprehension

--- Week 5 Files/word_occurrences.py
+++ Week 5 Files/word_occurrences.py
@@ -9,10 +9,10 @@ Github link: https://github.com/kkw123/CP1404Practicals
 
 def main():
     user_input = input('Enter a string: ').strip()
-    counts_per_word = check_string(user_input)
-    longest_word = check_longest_word(counts_per_word)
-    for word, count in counts_per_word.items():
-        print('{:{}} = {}'.format(word, len(longest_word), count))
+    word_to_counts = check_string(user_input)
+    longest_word_length = max([len(word) for word in word_to_counts.keys()])
+    for word, count in word_to_counts.items():
+        print('{:{}} = {}'.format(word, longest_word_length , count))
 
 
 def check_string(string1):
@@ -24,14 +24,6 @@ def check_string(string1):
         else:
             word_count[word] += 1
     return word_count
-
-
-def check_longest_word(dict1):
-    longest_word = str()
-    for word in dict1:
-        if len(word) > len(longest_word):
-            longest_word = word
-    return longest_word
 
 
 main()
