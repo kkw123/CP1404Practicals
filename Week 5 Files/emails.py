a7dd96204c65d4c561cc865163614b2b65b75d09
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-16 20:23:28 +0800

    Add planned process for file

---
+++ Week 5 Files/emails.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 Practical
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+
+"""
+
+
+def main():
+    # ask user for email
+    # convert text in front of @ into a name
+    # ask user if name is correct
+    # if True add into dictionary, else ask for real name and store both
+    pass
