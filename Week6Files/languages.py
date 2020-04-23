8ab103a4c736349238ff0165e7ce880f609404df
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-23 16:13:13 +0800

    Add import and initialise 3 languages

--- Week6Files/languages.py
+++ Week6Files/languages.py
@@ -5,6 +5,12 @@ Name: Kum King Wye
 Github link: https://github.com/kkw123/CP1404Practicals
 
 """
+from Week6Files.programming_language import ProgrammingLanguage
 
 def main():
-    pass
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+
+main()
