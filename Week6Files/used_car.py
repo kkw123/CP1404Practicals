b3c39db5315f53c81df16d08614efbb7ac7d6948
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-23 15:55:25 +0800

    Add lines for making the 'Limo' car

--- Week6Files/used_car.py
+++ Week6Files/used_car.py
@@ -8,6 +8,7 @@ from Week6Files.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
+    """
     my_car = Car(180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
@@ -16,6 +17,23 @@ def main():
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+    """
+
+    """Create limo Car object"""
+    limo = Car('Limo',100)
+    """Create limo with 100 fuel"""
+    limo.add_fuel(20)
+    """Add 20 more units of fuel"""
+    print('Fuel =', limo.fuel)
+    """Print amount of fuel in limo"""
+    limo.drive(115)
+    """Attempt to dive 115km"""
+    print('Odo =', limo.odometer)
+    """Print limo odometer"""
+    print(limo)
+    """Print Limo details"""
+
+
 
 
 main()
