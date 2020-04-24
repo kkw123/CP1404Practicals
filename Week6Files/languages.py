445511808159319b9bb27367e0b5f5b14fd97967
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-24 12:01:02 +0800

    Add a print statement for all dynamically typed languages

--- Week6Files/languages.py
+++ Week6Files/languages.py
@@ -7,10 +7,15 @@ Github link: https://github.com/kkw123/CP1404Practicals
 """
 from Week6Files.programming_language import ProgrammingLanguage
 
+
 def main():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-
+    languages = [ruby, python, visual_basic]
+    print('The dynamically typed languages are:')
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name.title())
 
 main()
