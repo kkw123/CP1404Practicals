3ce3e715facdb8382f34943d129b53e6ce126353
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-24 12:06:03 +0800

    Add guitar class with initial and string

---
+++ Week 4 Files/guitar.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+
+"""
+
+
+class Guitar:
+    def __init__(self, name='', year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return '{} ({}) : ${:,}'.format(self.name, self.year, self.cost)
