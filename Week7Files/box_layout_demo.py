8bdb8f59ae0fe91057ae07eb4eaf0e8517e54737
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-30 18:23:32 +0800

    Add functions to input name, clear input and print greetings

--- Week7Files/box_layout_demo.py
+++ Week7Files/box_layout_demo.py
@@ -18,4 +18,13 @@ class BoxLayoutDemo(App):
         return self.root
 
 
+    def handle_greet(self):
+        print("test")
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+
+    def clear_input(self):
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = 'Hello '
+
 BoxLayoutDemo().run()
