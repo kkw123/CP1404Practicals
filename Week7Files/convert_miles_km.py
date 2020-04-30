f0e10f63fcdb6e1714c81b25f8309422faf8d916
KingW <kingwye.kum@my.jcu.edu.au>
2020-04-30 20:53:24 +0800

    Convert the file into a kv file

--- Week7Files/convert_miles_km.py
+++ Week7Files/convert_miles_km.py
@@ -6,8 +6,18 @@ Github link: https://github.com/kkw123/CP1404Practicals
 
 """
 
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+
+class MilesToKilometersApp(App):
+    """ SquareNumberApp is a Kivy App for squaring a number """
+    def build(self):
+        Window.size = (200, 100)
+        self.title = "Convert miles to kilometers"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+
+MilesToKilometersApp.run()
