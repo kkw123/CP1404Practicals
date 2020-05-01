c0f5db26795bf741ff28ae87dc66d35cc3527ac9
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-01 15:34:46 +0800

    Add labels to show up depending on how many names are stored in the class

--- Week7Files/dynamic_labels.py
+++ Week7Files/dynamic_labels.py
@@ -6,9 +6,25 @@ Github link: https://github.com/kkw123/CP1404Practicals
 
 """
 
-
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.properties import StringProperty
 
+
+class DynamicLabelsApp(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ['Tom', 'Dick', 'Harry']
+
+    def build(self):
+        self.title = 'Dynamic'
+        self.root = Builder.load_file('dynamic_labels.kv')
+        for name in self.names:
+            temp_label = Label(text=name, id=name)
+            self.root.ids.entries_box.add_widget(temp_label)
+        return self.root
+
+
+DynamicLabelsApp().run()
