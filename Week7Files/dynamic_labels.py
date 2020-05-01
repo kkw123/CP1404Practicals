310b49d4b4863d91ac946bde8a284e2931c545e4
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-01 14:06:09 +0800

    Initial commit for dynamic_labels files

---
+++ Week7Files/dynamic_labels.py
@@ -0,0 +1,14 @@
+"""
+CP1404/CP5632 Practical
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+
+"""
+
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
