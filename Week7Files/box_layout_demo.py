1744032b3f4b9002d713848fe79f6bd89030874c
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-01 12:22:59 +0800

    Change box layout according to prac 07 and added github information

--- Week7Files/box_layout_demo.py
+++ Week7Files/box_layout_demo.py
@@ -10,6 +10,14 @@ Github link: https://github.com/kkw123/CP1404Practicals
 from kivy.app import App
 from kivy.lang import Builder
 
+"""
+CP1404/CP5632 Practical
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+
+"""
+
 
 class BoxLayoutDemo(App):
     def build(self):
@@ -17,14 +25,13 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
-
     def handle_greet(self):
         print("test")
         self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
 
-
     def clear_input(self):
         self.root.ids.input_name.text = ''
         self.root.ids.output_label.text = 'Hello '
+
 
 BoxLayoutDemo().run()
