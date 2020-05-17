145f3c98dab3344c43d8bad04876a27aaa5954f9
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-17 17:14:54 +0800

    Add code to test taxi class in taxi_test.py

--- Week8Files/taxi_test.py
+++ Week8Files/taxi_test.py
@@ -6,6 +6,23 @@ Github link: https://github.com/kkw123/CP1404Practicals
 
 """
 
+from Week8Files.taxi import Taxi
+
 
 def main():
-    pass
+    taxi1 = Taxi('Prius 1', 100, 1.23)
+    print(taxi1)
+    """First Details"""
+    taxi1.drive(40)
+    print(taxi1)
+    print('Current Fare is: ${}'.format(taxi1.get_fare()))
+    """Second Details after 40km driven"""
+    taxi1.start_fare()
+    taxi1.drive(100)
+    print(taxi1)
+    print('Current Fare is: ${}'.format(taxi1.get_fare()))
+    """Third Details after odo reset and 100km drive"""
+
+
+    
+main()
