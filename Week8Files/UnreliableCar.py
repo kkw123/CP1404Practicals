71a9b01849a14515576c5ffbad88fdfd785f426e
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-17 17:49:18 +0800

    Initial commit of unreliable_car_test.py and UnreliableCar.py

---
+++ Week8Files/UnreliableCar.py
@@ -0,0 +1,14 @@
+"""
+CP1404/CP5632 Practical
+Car class
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+
+"""
+from Week8Files.car import Car
+
+
+class UnreliableCar(Car):
+    pass
+
---
+++ Week8Files/unreliable_car_test.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 Practical
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+
+"""
+
+from Week8Files.UnreliableCar import UnreliableCar
+
+
+def main():
+    pass
+
+
+main()
