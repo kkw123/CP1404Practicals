4585f58e10828250c0c4162179e0b894de584881
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-17 22:33:59 +0800

    Add menu with input checker

--- Week8Files/taxi_simulator.py
+++ Week8Files/taxi_simulator.py
@@ -11,7 +11,20 @@ from Week8Files.SilverServiceTaxi import SilverServiceTaxi
 
 
 def main():
-    pass
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    user_input = str()
+    while not user_input.lower() == 'q':
+        print('Let\'s Drive')
+        print('q)uit, c)hoose taxi, d)rive')
+        user_input = input('>>> ')
+        if user_input.lower() == 'q':
+            pass
+        elif user_input.lower() == 'c':
+            pass
+        elif user_input.lower() == 'q':
+            pass
+        else:
+            print('Wrong Input')
 
 
 main()
