ac9d357a177d3a432d858a19a0b12f107500ea00
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-17 19:40:03 +0800

    Add initial function and a drive function with random number to check reliability

--- Week8Files/UnreliableCar.py
+++ Week8Files/UnreliableCar.py
@@ -7,8 +7,27 @@ Github link: https://github.com/kkw123/CP1404Practicals
 
 """
 from Week8Files.car import Car
+from random import randint
 
 
 class UnreliableCar(Car):
     pass
+
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.distance_driven = 0
+        self.odometer = 0
+        self.reliability = reliability
+        """Reliability is the chance for the car will actually drive"""
+
+    def drive(self, distance):
+        if randint(1, 100) <= self.reliability:
+            distance_driven = super().drive(distance)
+            return distance_driven
+        else:
+            return 0
+
+
+
+
 
