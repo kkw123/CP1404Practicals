46b30c13c146b0beac44813220d395bd7bdc10fc
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-17 21:07:52 +0800

    Add init, str and get fare functions

--- Week8Files/SilverServiceTaxi.py
+++ Week8Files/SilverServiceTaxi.py
@@ -10,4 +10,18 @@ from Week8Files.taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
-    pass
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(), SilverServiceTaxi.flagfall)
+
+    def get_fare(self):
+        return self.price_per_km * self.current_fare_distance + SilverServiceTaxi.flagfall
+
+
+
+
