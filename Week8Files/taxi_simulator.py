878ac8484b1dbcc26d28facbe2a58f525f596f03
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-17 22:29:04 +0800

    Initial commit for taxi simulator

---
+++ Week8Files/taxi_simulator.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+
+"""
+
+from Week8Files.taxi import Taxi
+from Week8Files.SilverServiceTaxi import SilverServiceTaxi
+
+
+def main():
+    pass
+
+
+main()
