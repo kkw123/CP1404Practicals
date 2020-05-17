e876d7997b2ec2ca58af936e8840f07bdd808bb7
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-17 23:01:25 +0800

    Add choose taxi function

--- Week8Files/taxi_simulator.py
+++ Week8Files/taxi_simulator.py
@@ -13,18 +13,34 @@ from Week8Files.SilverServiceTaxi import SilverServiceTaxi
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     user_input = str()
+    current_vehicle = None
     while not user_input.lower() == 'q':
         print('Let\'s Drive')
         print('q)uit, c)hoose taxi, d)rive')
         user_input = input('>>> ')
-        if user_input.lower() == 'q':
-            pass
+        if user_input.lower() == 'd':
+            if current_vehicle is None:
+                print('No vehicle chosen')
+            else:
+                pass
+            """Does drive function on the taxi"""
         elif user_input.lower() == 'c':
-            pass
+            current_vehicle = choose_taxi(taxis)
+            """Returns chosen taxi"""
         elif user_input.lower() == 'q':
             pass
-        else:
-            print('Wrong Input')
+            """Exits the loop"""
+
+
+def choose_taxi(array):
+    """Only breaks if valid Taxi chosen"""
+    print('Taxis Available')
+    taxi_count = 0
+    for taxi in array:
+        print('{} - {}'.format(taxi_count, taxi))
+        taxi_count += 1
+    choose_input = input('Choose taxi: ')
+    return choose_input
 
 
 main()
