b09608d90a4f4e790cd280ed1122cc10bfd18791
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-17 23:18:25 +0800

    Change class variable to match sample output in prac 08

--- Week8Files/taxi.py
+++ Week8Files/taxi.py
@@ -11,7 +11,7 @@ from Week8Files.car import Car
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
-    price_per_km = 1.23
+    price_per_km = 1.20
 
     def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
