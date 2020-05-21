7563ff3f7edef941cb2bf85b2c2629dab1823e22
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-21 21:12:20 +0800

    Add code to move each file to a folder according to its file extension

--- Week9Files/sort_files_1.py
+++ Week9Files/sort_files_1.py
@@ -26,19 +26,34 @@ def main():
                 pass
             else:
                 if file[letter] == '.':
-                    for array in range(letter + 1, 0):
-                        file_extension += file[array]
+                    for index in range(letter + 1, 0):
+                        file_extension += file[index]
         file_types.append(file_extension)
         file_extension = ''
     print(file_types)
 
     # create new directory with file_types
     for file_type in file_types:
+        print(file_type)
         try:
             os.mkdir(file_type)
         except FileExistsError:
             pass
 
+    # sort the files according to their file type
+    source_address = os.getcwd() + '\\'
+    destination_address = os.getcwd() + '\\'
+    for filename in files:
+        current_file_type = str()
+        for letter in range(0, len(filename)):
+            if filename[letter] == '.':
+                for index in range(letter + 1, len(filename)):
+                    current_file_type += filename[index]
+        source_address += filename
+        destination_address += current_file_type
+        shutil.move(source_address, destination_address)
+        source_address = os.getcwd() + '\\'
+        destination_address = os.getcwd() + '\\'
 
 
 main()
