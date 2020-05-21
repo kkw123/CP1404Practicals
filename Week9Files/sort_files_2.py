7ca94466de0a0698ba600f762969a2459abb5692
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-21 21:15:58 +0800

    Initial commit for sort_files_2.py

---
+++ Week9Files/sort_files_2.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 Practical
+Sort Files 2
+
+Name: Kum King Wye
+Github link: https://github.com/kkw123/CP1404Practicals
+"""
+import shutil
+import os
+
+
+def main():
+    pass
+
+
+main()
