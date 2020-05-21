e88f9cef4d32040c9364941b0ae46b28350724b8
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-21 20:23:52 +0800

    Add code to find all file types in the folder to sort

--- Week9Files/sort_files_1.py
+++ Week9Files/sort_files_1.py
@@ -12,8 +12,27 @@ import os
 def main():
     print("Starting directory is: {}".format(os.getcwd()))
     os.chdir('FilesToSort')
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-    for a in os.walk('.'):
-        print(a)
+    print("Files are {}".format(os.listdir('.')))
+    files = os.listdir('.')  # contains all file names
+    file_types = list()
+
+    # find all different file formats
+    file_type = str()
+    for file in files:
+        for letter in range(-1, -len(file), -1):  # starts searching from the back of file name
+            try:
+                file[letter - 1]
+            except IndexError:
+                pass
+            else:
+                if file[letter] == '.':
+                    for array in range(letter + 1, 0):
+                        file_type += file[array]
+        file_types.append(file_type)
+        file_type = ''
+    print(file_types)
+
+    # create new directory with file_types
+
 
 main()
