96236c10d8572e4e7377196f1aad57a20bf10822
KingW <kingwye.kum@my.jcu.edu.au>
2020-05-21 21:17:17 +0800

    Copy file type check from sort_files_1.py

--- Week9Files/sort_files_2.py
+++ Week9Files/sort_files_2.py
@@ -10,7 +10,27 @@ import os
 
 
 def main():
-    pass
+    print("Starting directory is: {}".format(os.getcwd()))
+    os.chdir('FilesToSort2')
+    print("Files are {}".format(os.listdir('.')))
+    files = os.listdir('.')  # contains all file names
+    file_types = list()
+
+    # find all different file formats
+    file_extension = str()
+    for file in files:
+        for letter in range(-1, -len(file), -1):  # starts searching from the back of file name
+            try:
+                file[letter - 1]
+            except IndexError:
+                pass
+            else:
+                if file[letter] == '.':
+                    for index in range(letter + 1, 0):
+                        file_extension += file[index]
+        file_types.append(file_extension)
+        file_extension = ''
+    print(file_types)
 
 
 main()
